c5ff4762e4cfcdda12411431af8c03f90ec106c2
Matias <matiasgc@gmail.com>
2021-01-22 09:21:46 -0300

    Agregado homeController y actualizado base.html.twig

---
+++ symfony_4/src/Controller/homeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Request\ParamFetcherInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ *  @Rest\Route("/inicio")
+ */
+
+ class homeController extends AbstractFOSRestController
+ {
+     /**
+      *     @Rest\Get("/inicio1")
+      */
+
+      public function inicio(Request $request){
+          return $this->render("base.html.twig", ["var" => "variable", "var2" => "variable2"]);
+      }
+ }
+
